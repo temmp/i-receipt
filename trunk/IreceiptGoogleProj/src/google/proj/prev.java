567737319b0c4b1d9ce4967ccbffee2f81a7aa14
idanbod@gmail.com <idanbod@gmail.com@dcdcb942-6ffe-b105-d6de-75bb3be1a20c>
2010-12-22 19:18:24 +0000

    toast "please wait for the OCR result"

--- trunk/IreceiptGoogleProj/src/google/proj/prev.java
+++ trunk/IreceiptGoogleProj/src/google/proj/prev.java
@@ -21,129 +21,125 @@ import java.io.IOException;
 
 // ----------------------------------------------------------------------
 
-public class prev extends Activity {    
-    private Preview mPreview;
-    private  BMcamera mCamera;
+public class prev extends Activity {
+	private Preview mPreview;
+	private BMcamera mCamera;
 
-    @Override
-        protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Hide the window title.
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //show instruction toast
-        Context context = getApplicationContext();
-        CharSequence text = "Touch when ready to take a picture!";
-        int duration = Toast.LENGTH_LONG;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL, 0, 0);
-        toast.show();
-        // Create our Preview view and set it as the content of our activity.
-        try{
-            mPreview = new Preview(this);
-            setContentView(mPreview);
-            mPreview.setOnClickListener(new OnClickListener() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Hide the window title.
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// show instruction toast
+		CharSequence text = "Touch when ready to take a picture!";
+		final Toast toast = Toast.makeText(getApplicationContext(), text,
+				Toast.LENGTH_LONG);
+		toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL,
+				0, 0);
+		toast.show();
+		// Create our Preview view and set it as the content of our activity.
+		try {
+			mPreview = new Preview(this);
+			setContentView(mPreview);
+			mPreview.setOnClickListener(new OnClickListener() {
 
-                @Override
-                public void onClick(View v) {
-                	//mCamera.takePicture(null, null, null, null);
-                	mCamera.release();
-        			Intent i = new Intent(prev.this, compute_receipt.class);
-        			Receipt rec = idan.rec_arr.get(getIntent().getFlags());
-        			rec.setFilepath("/data/1.jpg");//hard code the path for the emulator
-        			int index=idan.rec_arr.indexOf(rec);
-        			i.setFlags(index);
-    				startActivityForResult(i, index);
-                }
-            });
-            }
-        catch(RuntimeException e){
-            System.out.println(e.getMessage());
-        }
-        
-        Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
-    		public void onPictureTaken(byte[] imageData, Camera c) {
-    			
-    			
-/*
-    			if (imageData != null) {
+				@Override
+				public void onClick(View v) {
+					// mCamera.takePicture(null, null, null, null);
+					toast.setText("please wait for the OCR result...");
+					mCamera.release();
+					Intent i = new Intent(prev.this, compute_receipt.class);
+					Receipt rec = idan.rec_arr.get(getIntent().getFlags());
+					rec.setFilepath("/data/1.jpg");// hard code the path for the
+													// emulator
+					int index = idan.rec_arr.indexOf(rec);
+					i.setFlags(index);
+					startActivityForResult(i, index);
+				}
+			});
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+		}
 
-    				Intent mIntent = new Intent();
+		Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+			public void onPictureTaken(byte[] imageData, Camera c) {
 
-    				FileUtilities.StoreByteImage(mContext, imageData,
-    						 50, "ImageName");
-    				mCamera.startPreview();
-    				
-    				setResult(FOTO_MODE,mIntent);
-    				finish();
-    			
+				/*
+				 * if (imageData != null) {
+				 * 
+				 * Intent mIntent = new Intent();
+				 * 
+				 * FileUtilities.StoreByteImage(mContext, imageData, 50,
+				 * "ImageName"); mCamera.startPreview();
+				 * 
+				 * setResult(FOTO_MODE,mIntent); finish();
+				 * 
+				 * 
+				 * }
+				 */}
+		};
+	}
 
-    			}
-  */  		}
-    	};
-        
-}
-    
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// in case we cancel in compute_receipt
-		if (resultCode==0)
+		if (resultCode == 0)
 			setResult(0);
 		else
 			setResult(1);
 		finish();
-				
+	}
+
+	// ----------------------------------------------------------------------
+
+	class Preview extends SurfaceView implements SurfaceHolder.Callback {
+		SurfaceHolder mHolder;
+
+		// Camera mCamera;
+		Preview(Context context) {
+			super(context);
+
+			// Install a SurfaceHolder.Callback so we get notified when the
+			// underlying surface is created and destroyed.
+			mHolder = getHolder();
+			mHolder.addCallback(this);
+			// mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+			mHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
 		}
 
-	
+		public void surfaceCreated(SurfaceHolder holder) {
+			// The Surface has been created, acquire the camera and tell it
+			// where
+			// to draw.
+			// mCamera = Camera.open();
+			mCamera = BMcamera.open();
+			try {
+				mCamera.setPreviewDisplay(holder);
+			} catch (IOException exception) {
+				mCamera.release();
+				mCamera = null;
+				// TODO: add more exception handling logic here
+			}
+		}
 
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// Surface will be destroyed when we return, so stop the preview.
+			// Because the CameraDevice object is not a shared resource, it's
+			// very
+			// important to release it when the activity is paused.
+			mCamera.stopPreview();
+			mCamera.release();
+			mCamera = null;
+		}
 
-
-// ----------------------------------------------------------------------
-
-class Preview extends SurfaceView implements SurfaceHolder.Callback {
-    SurfaceHolder mHolder;
-    //Camera mCamera;
-    Preview(Context context) {
-        super(context);
-
-        // Install a SurfaceHolder.Callback so we get notified when the
-        // underlying surface is created and destroyed.
-        mHolder = getHolder();
-        mHolder.addCallback(this);
-        //mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
-    }
-
-    public void surfaceCreated(SurfaceHolder holder) {
-        // The Surface has been created, acquire the camera and tell it where
-        // to draw.
-    	//mCamera = Camera.open();
-        mCamera = BMcamera.open();
-        try {
-           mCamera.setPreviewDisplay(holder);
-        } catch (IOException exception) {
-            mCamera.release();
-            mCamera = null;
-            // TODO: add more exception handling logic here
-        }
-    }
-
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // Surface will be destroyed when we return, so stop the preview.
-        // Because the CameraDevice object is not a shared resource, it's very
-        // important to release it when the activity is paused.
-        mCamera.stopPreview();
-        mCamera.release();
-        mCamera = null;
-    }
-
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // Now that the size is known, set up the camera parameters and begin
-        // the preview.
-        Camera.Parameters parameters = mCamera.getParameters();
-        parameters.setPreviewSize(w, h);
-        mCamera.setParameters(parameters);
-        mCamera.startPreview();
-    }
-
-}
+		public void surfaceChanged(SurfaceHolder holder, int format, int w,
+				int h) {
+			// Now that the size is known, set up the camera parameters and
+			// begin
+			// the preview.
+			Camera.Parameters parameters = mCamera.getParameters();
+			parameters.setPreviewSize(w, h);
+			mCamera.setParameters(parameters);
+			mCamera.startPreview();
+		}
+	}
 }
