898c9c2346e8ccbd4271587e8960fbf146a8f869
hobotgil@gmail.com <hobotgil@gmail.com@dcdcb942-6ffe-b105-d6de-75bb3be1a20c>
2010-12-26 20:54:16 +0000

    drop down title sync dialog

--- trunk/IreceiptGoogleProj/src/google/proj/rec_list.java
+++ trunk/IreceiptGoogleProj/src/google/proj/rec_list.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 
 import google.proj.R;
 import android.view.View;
+import android.view.Window;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -28,6 +29,7 @@ public class rec_list extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.reclist);
 		myListView = (ListView) findViewById(R.id.ListView01);
 		// if we open from compute Receipt
--- trunk/IreceiptGoogleProj/src/google/proj/statistics.java
+++ trunk/IreceiptGoogleProj/src/google/proj/statistics.java
@@ -9,6 +9,7 @@ import java.util.Calendar;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import google.proj.R;
 
@@ -25,6 +26,7 @@ public class statistics extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.stats);
 		fromPickDate = (TextView) findViewById(R.id.pickDateFrom);
 		toPickDate = (TextView) findViewById(R.id.pickDateTo);
